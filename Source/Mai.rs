63d6e951101bc84a739c82f669d5faf914fcd408
Owen Gaydosz <owgydz@proton.me>
2025-02-18 21:25:24 -0500

    main: Update main to use the DayAndNight cycles

--- Source/Mai.rs
+++ Source/Mai.rs
@@ -3,10 +3,14 @@ use world::WorldPlugin;
 
 mod block;
 mod world;
+mod Crafting;
+mod Sal;
+mod DayAndNight;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(WorldPlugin)
+        .add_startup_system(DayAndNight::setup_day_night)
         .run();
 }
