8b6d8cbae00d535242c93c62aa0443d4fe952ff9
Owen Gaydosz <owgydz@proton.me>
2025-02-21 21:27:34 -0500

    blocks: fix incomplete header in blocks.c

--- Source/blocks.c
+++ Source/blocks.c
@@ -3,26 +3,25 @@
 #include "player.h"
 
 Block* get_block_at(int x, int y, int z) {
-    // Implement logic to retrieve the block at the given coordinates
     return find_block_in_world(x, y, z);
 }
 
 void destroy_block(Block* block) {
-    if (block && block->is_mineable) {
+    if (block && is_mineable(block)) {
         drop_item(block->type, block->x, block->y, block->z);
         block->type = BLOCK_AIR;
     }
 }
 
 void dig_block(Block* block) {
-    if (block && block->is_diggable) {
+    if (block && is_diggable(block)) {
         drop_item(block->type, block->x, block->y, block->z);
         block->type = BLOCK_AIR;
     }
 }
 
 void chop_block(Block* block) {
-    if (block && block->is_choppable) {
+    if (block && is_choppable(block)) {
         drop_item(block->type, block->x, block->y, block->z);
         block->type = BLOCK_AIR;
     }
@@ -36,5 +35,15 @@ void weaken_block(Block* block) {
         }
     }
 }
-//                 destroy_block(target_block);
-//             }
+
+int is_mineable(Block* block) {
+    return block && (block->type == STONE || block->type == WOOD || block->type == BLOCK_DIRT);
+}
+
+int is_diggable(Block* block) {
+    return block && (block->type == DIRT || block->type == SAND);
+}
+
+int is_choppable(Block* block) {
+    return block && (block->type == WOOD);
+}
